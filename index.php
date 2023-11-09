63a296214085e232b0c347362dbd163f208fd36c
NotBB <notice76610@gmail.com>
2023-11-09 11:50:44 +0100

    -

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Dossier TP</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+	<div class="hero-section">
+		<div class="about-text hover-target">devis</div>
+		<div class="contact-text hover-target">contact</div>
+		<div class="section-center">
+			<div class="container-fluid">
+				<div class="row justify-content-center">
+					<div class="col-12 text-center">
+						<h1>Moon<br>Prod</h1>
+					</div>
+					<div class="col-12 text-center mb-2">
+						<div class="dancing"><span>monteur</span></div>
+					</div>
+					<div class="col-12 text-center">
+						<p>
+							<span class="travel hover-target">1</span>
+							<span class="wildlife hover-target">2</span>
+							<span class="nature hover-target">3</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="about-section">
+		<div class="about-close hover-target"></div>
+		<div class="section-center">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-12 text-center">
+						<img src="not.png" alt="">
+					</div>
+					<div class="col-lg-8 text-center mt-3">
+						<p class="mb-1">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.</p>
+					</div>
+					<div class="col-12 text-center">
+						<p class="mb-0"><span>Christian Arete</span></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="contact-section">
+		<div class="contact-close hover-target"></div>
+		<div class="section-center">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-12 text-center">
+						<form class="form">
+							<h2>Contacte Moi !</h2>
+							<p type="Name:"><input placeholder="Nom et Prénom"></input></p>
+							<p type="Email:"><input placeholder="Votre adresse mail"></input></p>
+							<p type="Message:"><input placeholder="Votre message"></input></p>
+							<button>Send Message</button>
+							<div class="icone">
+								<span class="fa fa-phone"></span>001 1023 567
+								<span class="fa fa-envelope-o"></span> contact@company.com
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="travel-section">
+		<div class="travel-close hover-target"></div>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-12 text-center">
+					<h3>2</h3>
+				</div>
+				<div class="col-12 mt-3 text-center">
+					<p><span>Canon PowerShot S95</span></p>
+				</div>
+				<div class="col-12 text-center">
+					<p>
+						focal length: 22.5mm<br>
+						aperture: ƒ/5.6<br>
+						exposure time: 1/1000<br>
+						ISO: 80
+					</p>
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="wildlife-section">
+		<div class="wildlife-close hover-target"></div>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-12 text-center">
+					<h3>1</h3>
+				</div>
+				<div class="col-12 mt-3 text-center">
+					<p><span>Canon PowerShot S95</span></p>
+				</div>
+				<div class="col-12 text-center">
+					<p>
+						focal length: 22.5mm<br>
+						aperture: ƒ/5.6<br>
+						exposure time: 1/1000<br>
+						ISO: 80
+					</p>
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="nature-section">
+		<div class="nature-close hover-target"></div>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-12 text-center">
+					<h3>3</h3>
+				</div>
+				<div class="col-12 mt-3 text-center">
+					<p><span>Canon PowerShot S95</span></p>
+				</div>
+				<div class="col-12 text-center">
+					<p>
+						focal length: 22.5mm<br>
+						aperture: ƒ/5.6<br>
+						exposure time: 1/1000<br>
+						ISO: 80
+					</p>
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<img src="https://assets.codepen.io/1462889/photo-p.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class='cursor' id="cursor"></div>
+	<div class='cursor2' id="cursor2"></div>
+	<div class='cursor3' id="cursor3"></div>
+
+	<!-- Lien
+	================================================== -->
+
+	<a href="https://github.com/NotBB7" class="logo hover-target" target="_blank">
+		<img src="not.png" alt="">
+	</a>
+
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"></script>
+<script src="script.js"></script>
+
+</html>
